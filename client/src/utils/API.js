c66c298e18a6faffa3e8da998f8beb0aacd0aa84
Natalie Roberts <nr_roberts@yahoo.com>
2018-03-23 14:07:36 -0400

    requested new API key and redid query url

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,10 +1,15 @@
 import axios from "axios";
 
-const BASEURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=b9f91d369ff59547cd47b931d8cbc56b:0:74623931&q=";
+const api = {
+  getArticles: function (topic, startYear, endYear) {
 
-// Export an object with a "search" method that searches the NYT API for the passed query
-export default {
-  search: function(query) {
-    return axios.get(BASEURL + query);
+  const key = "b9f91d369ff59547cd47b931d8cbc56b:0:74623931"
+
+  const url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" +
+        key  + "&q=" + topic + "&begin_date=" + startYear + "0101&end_date=" + endYear + "0101";
+        console.log(url);
+        return axios.get(url);
   }
 };
+
+export default api;
