b6d702c13b3f853113182e507b635b73af1b76f4
Natalie Roberts <nr_roberts@yahoo.com>
2018-03-23 15:11:31 -0400

    added new API key

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = {
   getArticles: function (topic, startYear, endYear) {
 
-  const key = "b9f91d369ff59547cd47b931d8cbc56b:0:74623931"
+  const key = "2fb707bcff61440288915b104ddd63e5"
 
   const url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" +
         key  + "&q=" + topic + "&begin_date=" + startYear + "0101&end_date=" + endYear + "0101";
