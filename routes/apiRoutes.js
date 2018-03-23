c75a6efc36dd131c94f6ecc9a935f0645cca66e5
Natalie Roberts <nr_roberts@yahoo.com>
2018-03-23 15:35:44 -0400

    modified controller, apiRoutes, and index.js, routes still not working

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -7,18 +7,19 @@ module.exports = function (passport) {
 	//add any API routes here. * THESE ARE NOT WORKING *
 	const articleController = require("../controllers/article");
 
-
-	// GET route to load the single React HTML page.
-
 	// GET route to QUERY MongoDB for all saved articles.
-	router.findAll("/", articleController.findAll);
+		router.route("/")
+			.get(articleController.findAll)
 	// POST route to DELETE a saved article from the database.
-	// router.delete("/:id", articleController.remove);
+		// router.delete("/:id", articleController.remove);
+		router.route("/:id")
+			.post(articleController.remove)
 	// POST route to SAVE an article to the database.
-	// router.put("/:id", articleController.update);
+		// router.put("/:id", articleController.update);
+		router.route("/:id")
+			.put(articleController.update)
 	
-	// module.exports = router;
-
+	module.exports = router;
 
 	return router;
 };
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,14 @@
 module.exports = function (passport) {
 	const path = require("path");
 	const router = require('express').Router();
+	const apiRoutes = require("./apiRoutes.js");
 
 
 	router.use("/auth",require("./authRoutes.js")(passport));
-	// router.use("/api",require("./apiRoutes.js")(app));
+	router.use("/api", apiRoutes);
 
 
-	// If no API routes are hit, send the React app here:
+	// GET route to load the single React HTML page. If no API routes are hit, send the React app here:
 	router.use(function(req, res) {
 	  res.sendFile(path.join(__dirname, "../client/index.html"));
 	});
